a0e7f1464348e4e464bb529b05bc1a45e2206331
redskull422 <kofifa.pg@gmail.com>
2022-07-25 15:02:00 +0430

    Imported files

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div id="app">Hello World</div>
+  <home-page />
 </template>
 
 <script>
+import HomePage from "./components/home.vue";
+import "./assets/css/main.css";
 export default {
   name: "App",
+  components: {
+    HomePage,
+  },
 };
 </script>
 
